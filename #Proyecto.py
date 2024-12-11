aa95fd41632f9e176ef5d06f0a188e2c5346a1aa
ltw-19 <ldterrones.25@est.ucab.edu.ve>
2024-12-11 00:30:00 -0400

    Add files via upload

---
+++ #Proyecto.py
@@ -0,0 +1,15 @@
+#Proyecto
+#Integrantes:
+
+# def leer_archivo(nombre_de_archivo):
+#     #Se lee el archivo
+#         return
+
+def calculadro_de_puntajes(contraseña, patrones):
+    puntos = 0
+    minuscula = 0
+    tiene_minuscula = False
+    tiene_mayuscula = False
+    tiene_numero = False
+    tiene_simbolo = False
+    cantidad_simbolos = 0
