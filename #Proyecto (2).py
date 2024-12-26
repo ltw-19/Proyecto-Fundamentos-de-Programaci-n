895c32bfca847c48360e6bc0c38d2693d94d6f64
ltw-19 <ldterrones.25@est.ucab.edu.ve>
2024-12-26 11:42:11 -0400

    Add files via upload

---
+++ #Proyecto (2).py
@@ -0,0 +1,54 @@
+#Proyecto
+#Integrantes: 
+# Luis Terrones C.I:  
+# Sebastian Veloso  C.I: 32.153.454
+
+contraseña = input("Introduzca su contraseña: ")
+
+def calculadro_de_puntajes(contraseña):
+    while len(contraseña) < 8:
+        print("La contraseña tiene que tener como minimo 8 caracteres, introduzca de nuevo: ")
+    return contraseña
+
+def calculadora_de_puntajes(contraseña):
+    
+    puntos = 0
+    minuscula = "abcdefghijklmnñopqrstuvwxyz"
+    mayuscula = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    numeros = "0123456789"
+    simbolos = "@$%&*~`/!#"
+
+    tiene_minuscula = False
+    tiene_mayuscula = False
+    tiene_numero = False
+    tiene_simbolo = False
+    
+    cantidad_minuscu = 0
+    cantidad_mayusculas = 0
+    cantidad_numeros = 0
+    cantidad_simbolos = 0
+
+    #Se analizan los caracteres
+    for i in contraseña:
+        puntos += 1
+        if i in minuscula:
+            tiene_minuscula = True 
+            puntos += 1
+
+        if i in mayuscula:
+            tiene_mayuscula = True 
+            puntos += 1
+            
+        if i in numeros:
+            tiene_numero = True 
+            puntos += 1
+            
+            cantidad_simbolos 
+        if i in simbolos:
+            tiene_simbolos = True
+            puntos += 1
+    return puntos
+resultado = calculadora_de_puntajes(contraseña)
+print("Tu contraseña tiene ", resultado, "puntajes")
+
+
