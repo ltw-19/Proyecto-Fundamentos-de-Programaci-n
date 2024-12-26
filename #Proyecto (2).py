85041207a45a584c462b4ea0dfaab4e694aff336
ltw-19 <ldterrones.25@est.ucab.edu.ve>
2024-12-26 12:29:59 -0400

    Update #Proyecto (2).py

--- #Proyecto (2).py
+++ #Proyecto (2).py
@@ -4,7 +4,7 @@
 # Sebastian Veloso  C.I: 32.153.454
 
 contraseña = input("Introduzca su contraseña: ")
-
+puntos = 0
 def calculadora_de_puntajes(contraseña):
     while len(contraseña) < 8:
         contraseña = input("La contraseña tiene que tener como minimo 8 caracteres, introduzca de nuevo: ")
@@ -42,8 +42,26 @@ def calculadora_de_puntajes(contraseña):
             
         if i in simbolos:
             tiene_simbolo = True
-            puntos += 1
+            puntos += 3
+            cantidad_simbolos += 1
+            if cantidad_simbolos > 1:
+                puntos = 2 * (cantidad_simbolos - 1) #Aqui se realiza un incremento de la variable.
+                #En primer lugar se resta el valor de la cantidad de simbolos -1. Luego ese resultado se multiplica por 2.
+                #Luego ese valor resultante se suma al valor de puntos
+
     return puntos
 
-resultado = calculadora_de_puntajes(contraseña)
-print("Tu contraseña tiene ", resultado, "puntajes")
+#Esta funcion clasifica la contraseña segun el puntaje 
+def clasificacion_de_contraseñas(puntos):
+    if puntos >= 15:
+        return "Fuerte"
+    elif puntos >= 10:
+        return "Moderada"
+    elif puntos < 10:
+        return "Debil"
+    
+
+
+resultado_puntajes = calculadora_de_puntajes(contraseña)
+categoria = clasificacion_de_contraseñas(resultado_puntajes)
+print(contraseña, "|", resultado_puntajes, "puntajes", "|",  categoria)
